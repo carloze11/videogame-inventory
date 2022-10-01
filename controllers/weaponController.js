09c3a76ec3e8d425159196e71f471bb680490c2f
Carlos Estrada <cestrada1109@gmail.com>
2022-10-01 01:14:02 -0500

    Add update functionality to weapons

--- controllers/weaponController.js
+++ controllers/weaponController.js
@@ -181,11 +181,111 @@ exports.weapon_delete_post = (req, res) => {
 };
 
 // Display weapon update form on GET.
-exports.weapon_update_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: weapon update GET");
+exports.weapon_update_get = (req, res, next) => {
+  async.parallel(
+    {
+      weapon(callback){
+        Weapon.findById(req.params.id).populate("game").populate("category").exec(callback)
+      },
+      games(callback){
+        Game.find(callback)
+      },
+      categorys(callback){
+        Category.find(callback)
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err)
+      }
+      if (results.weapon == null) {
+        const err = new Error("Weapon not found")
+        err.status = 404
+        return next(err)
+      }
+
+      for (const category of results.categorys) {
+        for (const weaponCategory of results.weapon.category) {
+          if (category._id.toString() === weaponCategory._id.toString()) {
+            category.checked = "true"
+          }
+        }
+      }
+      res.render("weapon_form", {
+        title: "Update Weapon", 
+        games: results.games,
+        categorys: results.categorys,
+        weapon: results.weapons
+      })
+    }
+  )
 };
 
 // Handle weapon update on POST.
-exports.weapon_update_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: weapon update POST");
-};
+exports.weapon_update_post = [
+  (req, res, next) => {
+    if (!Array.isArray(req.body.category)){
+      req.body.category = typeof req.body.category === "undefined" ? [] : [req.body.category]
+    }
+    next();
+  },
+
+  body("name", "Name must not be empty.").trim().isLength({ min: 1}).escape(),
+  body("description", "Description must not be empty.").trim().isLength({ min: 1 }).escape(),
+  body("game", "Must choose a game.").trim().isLength({min: 1}).escape(),
+  body("category.*").escape(),
+  body("tier", "Select a tier.").trim().isLength({min: 1}).escape(),
+
+  (req, res, next) => {
+    const error = validationResult(req)
+    const weapon = new Weapon({
+      name: req.body.name,  
+      description: req.body.description, 
+      game: req.body.game,
+      category: typeof req.body.category === undefined ? [] : req.body.category,
+      tier: req.body.tier,
+      _id: req.params.id,
+    })
+
+    if (!error.isEmpty()) {
+      async.parallel(
+        {
+          weapon(callback){
+            Weapon.findById(req.params.id).populate("game").populate("category").exec(callback)
+          },
+          games(callback){
+            Game.find(callback)
+          },
+          categorys(callback){
+            Category.find(callback)
+          },
+        },
+        (err, results) => {
+          if (err) {
+            return next(err)
+          }
+
+          for (const category of results.categorys) {
+            if (weapon.category.includes(category._id)) {
+              category.checked = "true"
+            }
+          }
+          res.render("weapon_form", {
+            title: "Update Weapon", 
+            games: results.games,
+            categorys: results.categorys,
+            weapon: results.weapon,
+            errors: errors.array(),
+          })
+        }
+      )
+      return;
+    }
+    Weapon.findByIdAndUpdate(req.params.id, weapon, {}, (err, theweapon) => {
+      if (err){
+        return next(err)
+      }
+      res.redirect(theweapon.url)
+    })
+  }
+]
